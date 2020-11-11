facc30ccded0579f25c761bbaf1b2c71ed8b1a47
Bobbbay Bobbayan <abatterysingle@gmail.com>
2020-11-10 23:45:39 -0500

    Updated dots
    Signed-off-by: Bobbbay Bobbayan <abatterysingle@gmail.com>

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const int swallowfloating    = 1;
+static const int swallowfloating    = 0;
 static const char col_gray1[]       = "#af3a03";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
